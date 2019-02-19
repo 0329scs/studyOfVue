c5f6a5f29f86e4976d079380989d2161a983ef40
0329scs <0329scs@naver.com>
2019-02-19 17:54:46 +0900

    제이쿼리, 라이브러리 연동예제

---
+++ alone/examples/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    제이쿼리 / 라이브러리 연동<br>
+    <DatePicker></DatePicker><br>
+    <button type="button" class="btn btn-primary btn-lg">Large Button</button>
+    <Chart></Chart>
+  </div>
+</template>
+
+<script>
+import DatePicker from './DatePicker.vue'
+import Chart from './Chart.vue';
+export default {
+  components: {
+    DatePicker,
+    Chart
+  }
+}
+</script>
+
+<style>
+
+</style>
