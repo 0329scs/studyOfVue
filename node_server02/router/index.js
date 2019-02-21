a3c6159709ac180a5abe21ad07ce5a741f0b3f5e
0329scs <0329scs@naver.com>
2019-02-21 17:18:21 +0900

    뷰 게시판 스터디 4주차 복습 in 19.02.21

--- node_server02/router/index.js
+++ node_server02/router/index.js
@@ -3,7 +3,10 @@ const router = express.Router()
 
 const notice = require('./notice')
 
+const login = require('./login')
+
 router.use('/notice', notice)
 
+router.use('/login', login)
 
 module.exports = router
